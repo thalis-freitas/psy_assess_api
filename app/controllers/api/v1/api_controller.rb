14cee4e2820ac4eb83c54a1c0ae5e7a8c9387ebf
Thalis Freitas <ofthalis@gmail.com>
2024-04-12 03:06:09 -0300

    add generic error handling

--- app/controllers/api/v1/api_controller.rb
+++ app/controllers/api/v1/api_controller.rb
@@ -1,3 +1,8 @@
 class Api::V1::ApiController < ApplicationController
   include AuthService
+
+  rescue_from ActiveRecord::ActiveRecordError do |_exception|
+    render json: { error: I18n.t('errors.internal_server_error') },
+           status: :internal_server_error
+  end
 end
--- spec/requests/api/v1/evaluated_spec.rb
+++ spec/requests/api/v1/evaluated_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-describe 'Evaluated API', type: :request do
+describe 'Api::V1::EvaluatedController', type: :request do
   context 'GET /api/v1/evaluated' do
     it 'returns a list of evaluated users with their attributes' do
       create_list(:user, 5)
@@ -8,11 +8,21 @@ describe 'Evaluated API', type: :request do
       get '/api/v1/evaluated', headers: psychologist_token
 
       expect(response).to have_http_status(:success)
-      expect(json[:evaluated].count).to eql(5)
+      expect(json[:evaluated].count).to eq(5)
 
-      json[:evaluated].each do |load|
-        expect(load.keys).to include(*%i[name cpf email birth_date])
+      json[:evaluated].each do |e|
+        expect(e.keys).to include(*%i[name cpf email birth_date])
       end
+    end
+
+    it 'handles internal server error' do
+      allow(User).to receive(:where).and_raise(ActiveRecord::QueryCanceled)
+      create_list(:user, 5)
+
+      get '/api/v1/evaluated', headers: psychologist_token
+
+      expect(response).to have_http_status(:internal_server_error)
+      expect(json).to include(error: I18n.t('errors.internal_server_error'))
     end
   end
 end
