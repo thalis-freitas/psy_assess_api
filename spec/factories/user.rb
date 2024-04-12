96e441e4b180c29f7f01547223d6b3f9567f60f4
Thalis Freitas <ofthalis@gmail.com>
2024-04-12 01:39:13 -0300

    keep authentication data only for psychologists

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,9 +1,12 @@
 FactoryBot.define do
   factory :user do
+    name { Faker::Name.name }
+    cpf { Faker::IDNumber.brazilian_citizen_number }
     email { Faker::Internet.unique.email }
-    password { Faker::Lorem.word }
+    birth_date { Faker::Date.birthday(min_age: 18, max_age: 65) }
 
     trait :psychologist do
+      password { Faker::Lorem.word }
       role { :psychologist }
     end
   end
