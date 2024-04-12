b300a0fbd3101f2a6ecd8331f96cea4b52c486d7
Thalis Freitas <ofthalis@gmail.com>
2024-04-12 02:49:38 -0300

    assign default password as cpf for evaluated

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,12 @@ class User < ApplicationRecord
   has_secure_password
 
   enum role: { evaluated: 0, psychologist: 5 }
+
+  before_validation :update_default_password, if: :evaluated?
+
+  private
+
+  def update_default_password
+    self.password ||= cpf
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -10,5 +10,15 @@ RSpec.describe User, type: :model do
       user = User.new
       expect(user.role).to eq('evaluated')
     end
+
+    it 'should assign default password as cpf for evaluated' do
+      user = create(:user)
+      expect(user.authenticate(user.cpf)).to be_truthy
+    end
+
+    it 'should not assign default password as CPF for psychologists' do
+      user = create(:user, :psychologist)
+      expect(user.authenticate(user.cpf)).to be_falsey
+    end
   end
 end
