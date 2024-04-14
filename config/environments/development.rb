229c286a4ff9e8276c1b2d36e47ef4dda7cb3264
Thalis Freitas <ofthalis@gmail.com>
2024-04-14 17:58:17 -0300

    add Mailhog for email handling

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,6 +38,15 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+    address: 'mailhog',
+    port: 1025
+  }
+
+  config.action_mailer.default_options = { from: 'psy@assess.com' }
+
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
