a4268bedccce08d63567b72fb8d328639dcf1c93
Thalis Freitas <ofthalis@gmail.com>
2024-04-14 12:06:27 -0300

    add validation tests for evaluated role in evaluation

--- spec/models/evaluation_spec.rb
+++ spec/models/evaluation_spec.rb
@@ -24,4 +24,28 @@ RSpec.describe Evaluation, type: :model do
       expect(evaluation.status).to eq('pending')
     end
   end
+
+  describe '#valid' do
+    context 'evaluated must be valid role' do
+      it 'rejects evaluations where evaluated does not have evaluated role' do
+        psychologist = create(:user, :psychologist)
+        instrument = create(:instrument)
+
+        evaluation = Evaluation.new(evaluated: psychologist, instrument:)
+
+        expect(evaluation.valid?).to be false
+        expect(evaluation.errors[:evaluated])
+          .to include(I18n.t('errors.must_be_evaluated_role'))
+      end
+
+      it 'accepts evaluations where evaluated has evaluated role' do
+        evaluated = create(:user)
+        instrument = create(:instrument)
+
+        evaluation = Evaluation.new(evaluated:, instrument:)
+
+        expect(evaluation.valid?).to be true
+      end
+    end
+  end
 end
