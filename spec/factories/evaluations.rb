c8879f42901859f82d7f0ffeda60eb8b7bf8320c
Thalis Freitas <ofthalis@gmail.com>
2024-04-15 16:41:20 -0300

    update factories and seeds

--- spec/factories/evaluations.rb
+++ spec/factories/evaluations.rb
@@ -2,7 +2,17 @@ FactoryBot.define do
   factory :evaluation do
     evaluated { association :user }
     instrument
-    # score { 1 }
-    # token { 'MyString' }
+    status { 'pending' }
+
+    trait :sent do
+      status { 'sent' }
+      token { SecureRandom.hex(10) }
+    end
+
+    trait :finished do
+      status { 'finished' }
+      score { rand(1..10) } # Supõe que a pontuação máxima seja 10
+      token { SecureRandom.hex(10) }
+    end
   end
 end
--- spec/factories/questions.rb
+++ spec/factories/questions.rb
@@ -1,14 +1,21 @@
 FactoryBot.define do
   factory :question do
-    text { "Question #{Faker::Lorem.question}" }
+    text { Faker::Lorem.question }
     instrument
 
     trait :with_options do
       after(:create) do |question|
-        create(:option, question:, score_value: 3)
-        create(:option, question:, score_value: 2)
-        create(:option, question:, score_value: 1)
-        create(:option, question:, score_value: 0)
+        create(:option, question:, score_value: 3,
+                        text: Faker::Lorem.sentence(word_count: 2))
+
+        create(:option, question:, score_value: 2,
+                        text: Faker::Lorem.sentence(word_count: 2))
+
+        create(:option, question:, score_value: 1,
+                        text: Faker::Lorem.sentence(word_count: 2))
+
+        create(:option, question:, score_value: 0,
+                        text: Faker::Lorem.sentence(word_count: 2))
       end
     end
   end
